a2d7ef534d0fd4508780fe00888d9e465f022703
Michael <30883926+mgalde@users.noreply.github.com>
2018-09-08 21:04:02 -0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     url(r'^session', csrf_exempt(controllers.Session.as_view())),
     url(r'^register', csrf_exempt(controllers.Register.as_view())),
     url(r'^events', csrf_exempt(controllers.Events.as_view())),
-    url(r'^activateifttt', csrf_exempt(controllers.ActivateIFTTT.as_view())),
     url(r'^', include(router.urls)),
 ]
